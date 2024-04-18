a245d300cdd565ba62c07d516b8432a4f3a50b8f
Rafi <rafinaya1127@gmail.com>
2024-04-18 12:37:04 +0700

    halo bro ada commit baru

---
+++ Landmarks/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  Landmarks
+//
+//  Created by student on 18/04/24.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
