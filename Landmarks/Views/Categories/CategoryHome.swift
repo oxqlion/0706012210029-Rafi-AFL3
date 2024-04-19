1e8e44a181f83bc36af57a540b91c78aa2ae4970
Rafi <rafinaya1127@gmail.com>
2024-04-19 10:08:34 +0700

    jumat stage 3 don

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,51 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by student on 19/04/24.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    
+    @Environment(ModelData.self) var modelData
+    @State private var showingProfile = false
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+            }
+            .listStyle(.inset)
+            .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environment(ModelData())
+            }
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
